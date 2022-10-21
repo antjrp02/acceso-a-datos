dd4ac2b835cfadf5574a013e289d4f5a20e7f4fb
antjrp02 <92522333+antjrp02@users.noreply.github.com>
2022-10-21 13:34:50 +0200

    find by id

--- prZoologicoDAO/src/prZoologicoDAO/AnimalDAO.java
+++ prZoologicoDAO/src/prZoologicoDAO/AnimalDAO.java
@@ -2,13 +2,51 @@ package prZoologicoDAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class AnimalDAO {
 
 	private static Connection connection;
-	
+
+	public static Animal findById(int id) {
+		connection = openConnection();
+
+		String query = "select * from animales where id = ?";
+		Animal animal=null;
+
+		try {
+			PreparedStatement ps = connection.prepareStatement(query);
+			ps.setInt(1, id);
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()){
+				animal = new Animal(rs.getInt("id"),rs.getString("nombre"),rs.getString("habitat"),rs.getDouble("peso_aproximado")
+						);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return animal;
+	}
+
+	public static void deleteAnimalByNombre(String nombre) {
+		connection = openConnection();
+
+		String query = "delete from animales where nombre = ?";
+		try {
+			PreparedStatement ps = connection.prepareStatement(query);
+			ps.setString(1, nombre);
+			ps.executeUpdate();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		closeConnection();
+	}
+
 	public static void deleteAnimal() {
 		connection = openConnection();
 		String query = "delete from animales";
@@ -20,46 +58,46 @@ public class AnimalDAO {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		closeConnection();
 	}
+
 	public static void insertarAnimal(Animal a) {
 		connection = openConnection();
-		
-		String query="insert into animales(nombre,habitat,peso_aproximado) values(?,?,?); ";
+
+		String query = "insert into animales(nombre,habitat,peso_aproximado) values(?,?,?); ";
 		try {
 			PreparedStatement preparedStatement = connection.prepareStatement(query);
 			preparedStatement.setString(1, a.getNombre());
 			preparedStatement.setString(2, a.getHabitat());
 			preparedStatement.setDouble(3, a.getPeso_aproximado());
-			
+
 			preparedStatement.executeUpdate();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+
 	}
+
 	private static Connection openConnection() {
 		DatabaseConnection dbConnection = new DatabaseConnection();
 		connection = dbConnection.getConnection();
 		return connection;
-		
+
 	}
-	
+
 	private static Connection closeConnection() {
-		
+
 		try {
 			connection.close();
-			connection=null;
+			connection = null;
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return connection;
-		
+
 	}
-	
-	
+
 }
--- prZoologicoDAO/src/prZoologicoDAO/Main.java
+++ prZoologicoDAO/src/prZoologicoDAO/Main.java
@@ -5,8 +5,12 @@ public class Main {
 	public static void main(String[] args) {
 		/**Animal a =new Animal("Ardilla","Bosque",0.1);
 		AnimalDAO.insertarAnimal(a);
-		*/
+		
 		AnimalDAO.deleteAnimal();
+		*/
+		
+		Animal a1 = AnimalDAO.findById(2);
+		System.out.println(a1);
 	}
 
 }
